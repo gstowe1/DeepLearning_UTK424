f11ee96973401bbaaacfabda9edaffe2b9aa6c11
Gabriel <gstowe1@vols.utk.edu>
2023-05-10 13:58:10 -0400

    This uses just a lstm, which performs better than the research paper model

---
+++ finalProject/cnn_lstm_remastered.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+
+class CNN_LSTM:
+    def __init__(self):
+        pass
+
+    def input(self, name=None):
+        input = tf.keras.layers.Input(shape=(12,1), name=name)
+        return input
+
+    def conv(self, name=None):
+        conv = tf.keras.layers.Conv1D(filters=5, kernel_size=1, name=name, activation="relu")
+        return conv
+
+    def max(self, name=None):
+        return tf.keras.layers.MaxPool1D(name=name)
+
+    def lstm(self, name=None, return_seq=False):
+        lstm = tf.keras.layers.LSTM(5, name=name, return_sequences=return_seq, activation='relu')
+        return lstm
+
+    def fc(self, name=None):
+        return tf.keras.layers.Dense(1,activation='linear' ,name=name)
+
+    def create_model(self):
+        input = self.input("Input Layer")
+      
+        lstm1 = self.lstm("LSTM1")(input)
+    
+        fc = self.fc("FC")(lstm1)
+        model = tf.keras.models.Model(inputs=input, outputs=fc)
+
+        return model
