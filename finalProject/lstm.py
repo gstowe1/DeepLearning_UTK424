fbf9b41d28ff0d31623203575764b85e9fffbbe3
Gabriel <gstowe1@vols.utk.edu>
2023-05-13 20:14:06 -0400

    Removed unneeded function in the class.

--- finalProject/lstm.py
+++ finalProject/lstm.py
@@ -8,23 +8,13 @@ class _MODEL:
         input = tf.keras.layers.Input(shape=(12,1), name=name)
         return input
 
-    def conv(self, name=None):
-        conv = tf.keras.layers.Conv1D(filters=5, kernel_size=1, name=name, activation="relu")
-        return conv
-
-    def max(self, name=None):
-        return tf.keras.layers.MaxPool1D(name=name)
-
     def lstm(self, name=None, return_seq=False):
         lstm = tf.keras.layers.LSTM(5, name=name, return_sequences=return_seq, activation='relu')
         return lstm
 
     def fc(self, name=None):
         return tf.keras.layers.Dense(1,activation='linear' ,name=name)
-    
-    def dropout(self, name=None, amount=0.025):
-        dropout = tf.keras.layers.Dropout(amount)
-        return dropout
+
 
     def create_model(self):
         input = self.input("Input Layer")
